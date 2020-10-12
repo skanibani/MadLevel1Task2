1620c3760098d1c222800073acc21133527878fe
Skander <skander@bibani.nl>
2020-10-12 20:49:14 +0200

    Add counting correct answers

--- app/src/main/java/com/example/madlevel1task2/MainActivity.kt
+++ app/src/main/java/com/example/madlevel1task2/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.madlevel1task2
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -37,7 +38,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     /**
-     * Check inputs with according to AND conjunction
+     * Check inputs according to AND conjunction
      */
     private fun checkAnswer() {
 
@@ -47,10 +48,25 @@ class MainActivity : AppCompatActivity() {
         answers[2] = parseAnswer(binding.row3Input.text.toString())
         answers[3] = parseAnswer(binding.row4Input.text.toString())
 
+        var correctAnswers: Int = 0
+
+        if (answers[0]) {
+            correctAnswers++
+        }
+        if (!answers[1]) {
+            correctAnswers++
+        }
+        if (!answers[2]) {
+            correctAnswers++
+        }
+        if (!answers[3]) {
+            correctAnswers++
+        }
+
         if (answers[0] && !answers[1] && !answers[2] && !answers[3]) {
             Toast.makeText(this, getString(R.string.toast_correct), Toast.LENGTH_SHORT).show()
         } else {
-            Toast.makeText(this, getString(R.string.toast_incorrect), Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, getString(R.string.toast_incorrect, correctAnswers), Toast.LENGTH_SHORT).show()
         }
     }
 }
